674707e5712e46c2e55921bd4fd0b74f9b2219fe
kylerobins <robinsmicky@gmail.com>
2024-01-16 13:38:15 +0300

    Java Variables Examples

---
+++ 01. Intro To Java/2 Java Variables/Variables.java
@@ -0,0 +1,9 @@
+public class Variables {
+    public static void main(String[] args) {
+        int x, y, z = 50; // one value for different variables
+        int e = 23, f = 34, g = 45; // variables of the same type declared in one line
+        String myName = "Kyle Robins"; // String variable Declaration
+        boolean myBool = true; // boolean declaration
+        System.out.println(myName);
+    }
+}
