92d2a4c32d5658d2f0e6c6e88fcbf0a89216d76b
kylerobins <robinsmicky@gmail.com>
2024-01-16 13:11:40 +0300

    Java Syntax Example

---
+++ 01. Intro To Java/1 Java Syntax/Example.java
@@ -0,0 +1,8 @@
+public class Example { // The class should be the same name with the filename
+    public static void main(String[] args) {
+        String myName = "Kyle Robins"; // Declaring a variable of type String
+        System.out.println(myName); // prints the string
+        System.out.println(234); // prints intergers to the screen
+        System.out.print("Hello"); // prints on the same line
+    };
+}
